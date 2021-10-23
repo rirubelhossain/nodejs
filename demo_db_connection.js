6213c45391a524d85d45533bfd25c172891e0a4a
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2021-10-24 00:54:59 +0600

    how to create database and table over the mysql database using nodejs

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -1,3 +1,4 @@
+/*
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
@@ -9,4 +10,42 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+});
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+ 
+  con.query("CREATE DATABASE mydb_node", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+});
+*/
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mydb_node"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  /*Create a table named "customers":*/
+  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255) , email varchar(25))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
 });
